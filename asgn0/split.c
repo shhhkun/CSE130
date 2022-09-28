9c7c691a67c1be6449f3425e8c172e1232004893
Serjo <sepbarro@ucsc.edu>
2022-09-28 15:23:23 -0700

    added some error case msgs/warns

--- asgn0/split.c
+++ asgn0/split.c
@@ -4,9 +4,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <err.h>
 
 #define BLOCK  4096
-#define STDIN 0
+#define STDIN  0
 #define STDOUT 1
 
 int main(int argc, char *argv[]) {
@@ -14,44 +15,45 @@ int main(int argc, char *argv[]) {
     int count; // # of bytes for read/write calls
     uint8_t delim = (int) argv[argc - 1][0];
 
-    if (strlen(argv[argc - 1]) > 1) { // delimeter is longer than 1 char
-        printf("Delimiter invalid\n");
+    if (argc < 3) { // not enough args
+        fprintf(stderr, "split: not enough arguments\n"
+                        "usage: ./split <files> <delimiter>\n");
         return 0;
     }
 
-    if (argc == 1) { // no file parameters given
-        printf("No files given\n");
+    if (strlen(argv[argc - 1]) > 1) { // delimiter is longer than 1 char
+        fprintf(stderr, "split: only single character delimeters allowed\n"
+                        "usage: ./split <files> <delimiter>\n");
         return 0;
-    } // otherwise params given
+    }
 
     for (int i = 1; i < argc - 1; i += 1) {
         if (argv[i][0] == '-' && strlen(argv[i]) == 1) { // if file is '-' take input from stdin
-		while ((count = read(STDIN, buf, BLOCK)) > 0) {
-			for (int i = 0; i < count; i += 1) {
-				if (buf[i] == delim) {
-					buf[i] = '\n';
-				}
-			}
-			write(STDOUT, buf, count);
-		}
-        } else {
-		int infile = open(argv[i], O_RDONLY);
+            while ((count = read(STDIN, buf, BLOCK)) > 0) {
+                for (int i = 0; i < count; i += 1) {
+                    if (buf[i] == delim) {
+                        buf[i] = '\n';
+                    }
+                }
+                write(STDOUT, buf, count);
+            }
+        } else { // otherwise open specified file
+            int infile = open(argv[i], O_RDONLY);
 
-		if (infile == -1) {
-			printf("Failed to open file\n");
-			return 0;
-		}
+            if (infile == -1) { // if file doesnt open
+                warn("%s", argv[i]);
+            }
 
-		while ((count = read(infile, buf, BLOCK)) > 0) {
-			for (int i = 0; i < count; i += 1) {
-				if (buf[i] == delim) {
-					buf[i] = '\n';
-				}
-                	}
-			write(STDOUT, buf, count);
-		}
-		close(infile);
-	}
+            while ((count = read(infile, buf, BLOCK)) > 0) {
+                for (int i = 0; i < count; i += 1) {
+                    if (buf[i] == delim) {
+                        buf[i] = '\n';
+                    }
+                }
+                write(STDOUT, buf, count);
+            }
+            close(infile);
+        }
     }
     return 0;
 }
