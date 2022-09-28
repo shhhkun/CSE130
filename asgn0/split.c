adb435e62a3740adf890259f2ff566948c2f6505
Serjo <sepbarro@ucsc.edu>
2022-09-28 10:49:17 -0700

    added cases for stdin and multiple recognition, adding proper error msgs soon

--- asgn0/split.c
+++ asgn0/split.c
@@ -5,40 +5,53 @@
 #include <fcntl.h>
 #include <string.h>
 
-#define BLOCK 4096
+#define BLOCK  4096
+#define STDIN 0
 #define STDOUT 1
 
-int main (int argc, char  *argv[]) {
-	uint8_t buf[BLOCK];
-	int count; // # of bytes for read/write calls
-	int i = 1;
-	
-	if (argc == 1) { // no file parameters given so use stdin
-		printf("No files given\n");
+int main(int argc, char *argv[]) {
+    uint8_t buf[BLOCK];
+    int count; // # of bytes for read/write calls
+    uint8_t delim = (int) argv[argc - 1][0];
 
-		return 0;
+    if (strlen(argv[argc - 1]) > 1) { // delimeter is longer than 1 char
+        printf("Delimiter invalid\n");
+        return 0;
+    }
 
-	} // otherwise params given
+    if (argc == 1) { // no file parameters given
+        printf("No files given\n");
+        return 0;
+    } // otherwise params given
 
-	int infile = open(argv[i], O_RDONLY);
-
-	uint8_t delim = (int) argv[argc - 1][0];
-	
-	if (infile == -1) {
-		printf("Failed to open file\n");
-		return 0;
-	}
-	
-	while ((count = read(infile, buf, BLOCK)) > 0) {
-		for (int i = 0; i < count; i += 1) {
-			if (buf[i] == delim) {
-				buf[i] = '\n';
+    for (int i = 1; i < argc - 1; i += 1) {
+        if (argv[i][0] == '-' && strlen(argv[i]) == 1) { // if file is '-' take input from stdin
+		while ((count = read(STDIN, buf, BLOCK)) > 0) {
+			for (int i = 0; i < count; i += 1) {
+				if (buf[i] == delim) {
+					buf[i] = '\n';
+				}
 			}
+			write(STDOUT, buf, count);
 		}
-		write(STDOUT, buf, count);
+        } else {
+		int infile = open(argv[i], O_RDONLY);
+
+		if (infile == -1) {
+			printf("Failed to open file\n");
+			return 0;
+		}
+
+		while ((count = read(infile, buf, BLOCK)) > 0) {
+			for (int i = 0; i < count; i += 1) {
+				if (buf[i] == delim) {
+					buf[i] = '\n';
+				}
+                	}
+			write(STDOUT, buf, count);
+		}
+		close(infile);
 	}
-
-
-	close(infile);
-	return 0;
+    }
+    return 0;
 }
