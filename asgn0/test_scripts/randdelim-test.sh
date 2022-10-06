8679c61b82d0e51c5e9071a58154ed71aae73206
Serjo Barron <sepbarro@ucsc.edu>
2022-10-06 14:58:03 -0700

    changed file it tests

--- asgn0/test_scripts/randdelim-test.sh
+++ asgn0/test_scripts/randdelim-test.sh
@@ -1,3 +1,3 @@
-# Tests split using random delimiters on split itself
-text="split"
+# Tests split using random delimiters
+text="test_files/Wallpaper.webp"
 ./split $text $(cat /dev/random | head -c 1) > /dev/null
