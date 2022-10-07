f125102bcfbd83ab02d755cef7d9cbb4952cf07e
Serjo Barron <sepbarro@ucsc.edu>
2022-10-06 20:19:00 -0700

    fixed noperms test to echo proper return val and added diff to stdin test

--- asgn0/test_scripts/noperms-test.sh
+++ asgn0/test_scripts/noperms-test.sh
@@ -3,4 +3,11 @@ touch secret.txt
 chmod -r secret.txt
 texts="test_files/test0 secret.txt test_files/Wallpaper.webp"
 diff <(./split $texts x) <(./rsplit $texts x) > /dev/null
-rm secret.txt
+if [ $? = 0 ]
+then
+	rm secret.txt
+	exit 0
+else
+	rm secret.txt
+	exit 1
+fi
