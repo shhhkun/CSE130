40d84336122650f4e0b2f3c05d6cd9205e2cb322
Serjo <sepbarro@ucsc.edu>
2022-10-27 22:00:12 -0700

    fixed/added some error handling, and fixed creation of R/W perms in PUT req's

--- asgn1/httpserver.c
+++ asgn1/httpserver.c
@@ -56,13 +56,14 @@ void response(int connfd, char *resp, char *body, int code, int size) {
     if (body != NULL) {
         strcat(resp, body);
     }
-    send(connfd, resp, strlen(resp), 0);
+    write(connfd, resp, strlen(resp));
+    //send(connfd, resp, strlen(resp), 0);
     return;
 }
 
 void get(char *file, int connfd, char *buf, int size, char *resp) {
     int count;
-    int fd = open(file, O_RDONLY);
+    int fd = open(file, O_RDONLY); // change flag to RDWR and check if directory
     response(connfd, resp, NULL, 200, size); // 200 'OK'
     while ((count = read(fd, buf, 4096)) > 0) {
         write(connfd, buf, count);
@@ -79,6 +80,7 @@ void head(int connfd, int size, char *resp) {
 void put(char *file, int connfd, char *buf, char *header) {
     int content_len;
     int count, bytes_r = 0;
+
     char *ptr = strstr(header, "Content-Length:"); // move ptr to content len
     int code = 200; // default 200 'OK'
     if (ptr != NULL) {
@@ -87,10 +89,10 @@ void put(char *file, int connfd, char *buf, char *header) {
         }
         sscanf(ptr, "Content-Length: %d", &content_len);
 
-        int fd = open(file, O_CREAT | O_WRONLY | O_TRUNC);
+        int fd = open(file, O_CREAT | O_TRUNC + S_IRUSR + S_IWUSR);
+        //chmod(file, S_IRUSR + S_IWUSR);
 
-	// note: work on fixing read since it fails if content_len > buf size
-        while ((count = read(connfd, buf, content_len - bytes_r)) > 0) { // read request body
+        while ((count = read(connfd, buf, 4096)) > 0) { // read request body
             write(fd, buf, count); // write to replace/update file
             bytes_r += count; // bytes read
             if (content_len == bytes_r) {
@@ -114,8 +116,8 @@ int main(int argc, char *argv[]) {
     char body[4096];
     char resp[4096];
     char hfield[4096];
-    int port;
-    char method[8], filepath[4096], vers[9];
+    int port, errors = 0, not_rq = 1, malformed_header = 0; // 0 'false', 1 'true'
+    char method[2048], filepath[2048], vers[2048];
     char key[500], value[500];
     struct stat st;
 
@@ -134,14 +136,16 @@ int main(int argc, char *argv[]) {
 
     int listenfd = create_listen_socket(port);
 
-    if (listenfd == -3) { // change later
+    if (listenfd == -3) {
         warnx("bind: %s", strerror(errno));
         exit(1);
     }
 
     while (1) {
-        int errors = 0, not_rq = 1, malformed_header = 0; // 0 'false', 1 'true'
         int connfd = accept(listenfd, NULL, NULL); // wait for connection
+
+        errors = 0;
+        not_rq = 1;
 
         while ((read(connfd, buf, sizeof(char))) > 0) {
             if (not_rq == 0) {
@@ -150,51 +154,71 @@ int main(int argc, char *argv[]) {
             strcat(header, buf); // concatenate buf (request) into header
             char *start = strstr(header, "\r\n"); // end of request-line
             char *mid = strstr(hfield, "\r\n"); // end of a header-field
-            char *end = strstr(header, "\r\n\r\n"); // if end of request break
+            char *end = strstr(header, "\r\n\r\n"); // end of a request
             memset(&buf, 0, sizeof(buf));
-            if (mid != NULL && end == NULL) {
+            if (mid != NULL && end == NULL) { // found header-field
                 int kv = sscanf(hfield, "%s %s", key, value);
                 if (key[strlen(key) - 1] != ':' || kv != 2) {
                     malformed_header = 1;
                 }
                 memset(&hfield, 0, sizeof(hfield));
+                memset(&key, 0, sizeof(key));
+                memset(&value, 0, sizeof(value));
             }
-            if (end != NULL) {
+            if (end != NULL) { // found end of request
+                memset(&hfield, 0, sizeof(hfield));
                 break;
             }
-            if (start != NULL && not_rq == 1) {
+            if (start != NULL && not_rq == 1) { // found end of request-line
                 not_rq = 0;
             }
         }
 
         sscanf(header, "%s %s %s", method, filepath, vers); // parse request
-	char path[4096] = ".";
-	strcat(path, filepath);
-	char* filename = realpath(path, NULL); // get absolute path
+        //char path[4096] = ".";
+        //strcat(path, filepath);
+        //char *filename = realpath(path, NULL); // get absolute path
+        /*
+	printf("method = %s\n", method);
+	printf("path = %s\n", filepath);
+	printf("vers = %s\n", vers);
+	*/
+        char *filename = filepath + 1;
         int method_type = methodtype(method);
 
         stat(filename, &st);
         int size = st.st_size; // get file size (in bytes)
 
-        if ((strcmp(vers, "HTTP/1.1") != 0 || malformed_header == 1) && errors == 0) {
+        if ((strcmp(vers, "HTTP/1.1") != 0 || method[0] == '\0' || filepath[0] == '\0'
+                || vers[0] == '\0' || malformed_header == 1)
+            && errors == 0) {
             response(connfd, buf, "Bad Request\n", 400,
-                12); // if wrong HTTP vers or malformed header-fields
+                12); // if wrong HTTP vers, missing stuff, or malformed header-fields
             errors = 1;
+            malformed_header = 0;
         }
         if ((access(filename, F_OK) == -1 && errors == 0) && method_type != PUT) {
             response(
                 connfd, buf, "Not Found\n", 404, 10); // if file doesn't exist and not PUT request
             errors = 1;
         }
-        if ((access(filename, R_OK) == -1 && errors == 0) && method_type != PUT) {
-            response(connfd, buf, "Forbidden\n", 403, 10); // if no read perms and not PUT request
+        if (access(filename, F_OK) == 0) { // first check that file exists
+            if ((access(filename, R_OK) == -1 && errors == 0) && method_type != PUT) {
+                response(
+                    connfd, buf, "Forbidden\n", 403, 10); // if no read perms and not PUT request
+                errors = 1;
+            }
+            if ((access(filename, W_OK) == -1 && errors == 0) && method_type == PUT) {
+                response(
+                    connfd, buf, "Forbidden\n", 403, 10); // if no write perms and is PUT request
+                errors = 1;
+            }
+        }
+        if (S_ISDIR(st.st_mode) != 0 && errors == 0) {
+            response(connfd, buf, "Forbidden\n", 403, 10); // if filepath leads to directory
             errors = 1;
         }
-        if ((access(filename, W_OK) == -1 && errors == 0) && method_type == PUT) {
-            response(connfd, buf, "Forbidden\n", 403, 10); // if no write perms and is PUT request
-            errors = 1;
-        }
-        if (method_type != GET && method_type != HEAD && method_type != PUT) {
+        if (method_type != GET && method_type != HEAD && method_type != PUT && errors == 0) {
             response(connfd, buf, "Not Implemented\n", 501, 16); // if invalid method
             errors = 1;
         }
@@ -213,6 +237,9 @@ int main(int argc, char *argv[]) {
         memset(&header, 0, sizeof(header));
         memset(&body, 0, sizeof(body));
         memset(&resp, 0, sizeof(resp));
+        memset(&method, 0, sizeof(method));
+        memset(&filepath, 0, sizeof(filepath));
+        memset(&vers, 0, sizeof(vers));
 
         close(connfd);
     }
