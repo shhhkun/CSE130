5f90e4c4a081204b864a45f0bd8d08e74fd8fa35
Serjo <sepbarro@ucsc.edu>
2022-11-04 17:54:39 -0700

    adding .c file for empty queue block test

---
+++ asgn2/test_files/block-empty-queue.c
@@ -0,0 +1,64 @@
+#include "queue.h"
+
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *thread1(void *args) {
+    //printf("THREAD 1 BEGIN\n");
+    queue_t *q = ((void **) args)[0];
+
+    void *ret;
+    queue_pop(q, &ret); // pop from queue (will wait indefinitely)
+    if ((int) ret != 1) {
+	    return (void *) 1;
+    }
+
+    //printf("THREAD 1 FINISHED\n");
+    return NULL;
+}
+
+void *thread2(void *args) {
+    //printf("THREAD 2 BEGIN\n");
+    queue_t *q = ((void **) args)[0];
+
+    queue_push(q, (void *)1);
+
+    //printf("THREAD 2 FINISHED\n");
+    return NULL;
+}
+
+int main() {
+    queue_t *q = queue_new(10);
+    if (q == NULL) {
+        return 1;
+    }
+    // leave queue empty
+
+    pthread_t t1, t2;
+
+    void *args[1] = { q };
+
+    // pt_c(thread ad, NULL attr, func, func args)
+    pthread_create(&t1, NULL, thread1, args);
+
+    sleep(5);
+
+    pthread_create(&t2, NULL, thread2, args);
+
+    void *rc;
+
+    // close threads
+    pthread_join(t1, &rc);
+    // if rc "ret val"  != NULL, error code occurred
+    if (rc != NULL) {
+        return 1;
+    }
+    pthread_join(t2, &rc);
+    if (rc != NULL) {
+        return 1;
+    }
+
+    queue_delete(&q);
+    return 0;
+}
