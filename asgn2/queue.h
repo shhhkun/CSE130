af9ed234a9830733481451615dfc89542844db48
Serjo <sepbarro@ucsc.edu>
2022-11-04 18:41:20 -0700

    updating

--- asgn2/queue.h
+++ asgn2/queue.h
@@ -10,4 +10,3 @@ queue_t *queue_new(int size);
 void queue_delete(queue_t **q);
 bool queue_push(queue_t *q, void *elem);
 bool queue_pop(queue_t *q, void **elem);
-//void queue_print(queue_t *q); // rmv later
